2c02568739cddad9106ae6f4ac9f99088d2f77f8
Robbie Pallas <rpallas@gmail.com>
2014-02-25 19:18:33 +0000

    Fleshed out the login and refactored a little more

--- public/app/account/mvNavbarLoginCtrl.js
+++ public/app/account/mvNavbarLoginCtrl.js
@@ -1,11 +1,12 @@
-angular.module('app').controller('mvNavbarLoginCtrl', function($scope, $http) {
+angular.module('app').controller('mvNavbarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+    $scope.identity = mvIdentity;
     $scope.signin = function(username, password) {
-        $http.post('/login', {username: username, password: password })
-            .then(function(response){
-                if(response.data.success){
-                    console.log('login successful');
+        mvAuth.authenticateUser(username, password)
+            .then(function(success){
+                if(success){
+                    mvNotifier.notify('You have been successfully signed in');
                 } else {
-                    console.log('login failed');
+                    mvNotifier.notify('Login failed - Incorrect username or password');
                 }
             });
     }
---
+++ server/config/auth.js
@@ -0,0 +1,3 @@
+/**
+ * Created by robbiepallas on 25/02/2014.
+ */
