7e14b7291bd4b6d5534f9a1e7275b01211a4817e
Robbie Pallas <rpallas@gmail.com>
2014-03-05 18:58:20 +0000

    Configured jasmine style spec reporter to karma

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
