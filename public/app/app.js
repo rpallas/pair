a500e5598c710e030c22932a886810ab816aeb19
Robbie Pallas <rpallas@gmail.com>
2014-03-28 23:30:28 +0000

    'request-email' view now limited to authenticated users only

--- public/app/app.js
+++ public/app/app.js
@@ -32,7 +32,7 @@ angular.module('app').config(function($routeProvider, $locationProvider){
         .when('/users', { templateUrl: '/partials/users/user-list', controller: 'userListCtrl', resolve: routeRoleChecks.user })
         .when('/users/:id', { templateUrl: '/partials/users/user-detail', controller: 'userDetailCtrl', resolve: routeRoleChecks.user })
         .when('/dashboard', { templateUrl: '/partials/dashboard/dashboard', controller: 'dashboardCtrl', resolve: routeRoleChecks.user })
-        .when('/request-email', { templateUrl: '/partials/account/request-email', controller: 'signupCtrl' });
+        .when('/request-email', { templateUrl: '/partials/account/request-email', controller: 'signupCtrl', resolve: routeRoleChecks.user });
 
 });
 
