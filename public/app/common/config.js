ac78de71d5a4289ca918225feee5afbd6f1ff054
Robbie Pallas <rpallas@gmail.com>
2014-04-07 21:53:47 +0100

    Added some front end config

---
+++ public/app/common/config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+angular.module('app').factory('config', function(){
+    return {
+        blankProfileImage: "https://s3-eu-west-1.amazonaws.com/pair-app/blank-profile.png"
+    };
+});
