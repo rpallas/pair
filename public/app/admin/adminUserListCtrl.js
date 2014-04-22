0a68f4a581f766e9f66104b6146d6b5f6ce244f2
Robbie Pallas <rpallas@gmail.com>
2014-04-22 01:43:17 +0100

    added message for no users in the admin user list

--- public/app/admin/adminUserListCtrl.js
+++ public/app/admin/adminUserListCtrl.js
@@ -2,4 +2,8 @@
 
 angular.module('app').controller('adminUserListCtrl', function($scope, userResource){
     $scope.users = userResource.query();
+
+    $scope.hasUsers = function(){
+        return $scope.users.length > 0;
+    };
 });
