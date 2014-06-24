76a31fd355c33c8d13c962bd3483246e942c862f
Robbie Pallas <robbie.pallas@piksel.com>
2014-06-24 22:49:24 +0100

    added time list to request details view

--- public/app/requests/requestDetailCtrl.js
+++ public/app/requests/requestDetailCtrl.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('app').controller('requestDetailCtrl', function($scope, requestResource, $routeParams){
-    $scope.request = requestResource.get({_id: $routeParams.id});
 
+    $scope.request = requestResource.get({_id: $routeParams.id}, function (request) {
+        $scope.timeList = request.timeList;
+    });
 
 });
--- public/app/requests/time-list.directive.js
+++ public/app/requests/time-list.directive.js
@@ -1,17 +1,44 @@
 'use strict';
 
-angular.module('app').directive('pairTimeList', [
-    function() {
+angular.module('app').directive('pairTimeList',['config',
+    function(config) {
         return {
             templateUrl: '/partials/common/time-list',
             restrict: 'E',
+            scope: {
+                timeList: '=bind'
+            },
             link: function postLink(scope, element, attrs) {
+                scope.maxTimeListItems = config.maxTimeListItems;
+                scope.timeList = scope.timeList || [];
+                scope.time = "";
+
+                scope.addTime = function(){
+                    if(scope.canAddToTimeList()){
+                        scope.timeList.push(scope.time);
+                        scope.time = "";
+                    }
+                };
+
+                scope.canAddToTimeList = function () {
+                    return scope.timeList.length < scope.maxTimeListItems;
+                };
+
                 scope.hasTimes = function () {
                     return scope.timeList.length > 0;
                 };
-                scope.removeTimeSuggestion = function(time){
+
+                scope.removeTime = function(time){
                     var index = scope.timeList.indexOf(time);
                     scope.timeList.splice(index, 1);
+                };
+
+                scope.isEditMode = function () {
+                    return attrs.mode === "edit";
+                };
+
+                scope.acceptTime = function (time) {
+
                 };
             }
         };
